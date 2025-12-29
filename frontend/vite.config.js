f01ee40804df1c897be10fa42c04c366110c684c
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-29 14:51:55 +0330

    docker watch enabled on frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host:true
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
+    host: true,
+    strictPort: true,
   },
 });
